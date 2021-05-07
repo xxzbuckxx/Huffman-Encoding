d83455364ed004aaa66d519812e12132f68a7da9
Zack Traczyk <cookies951@gmail.com>
2021-05-07 11:29:48 -0500

    asgn5 graphs before/after encode

--- asgn5/.WRITEUP/plot.sh
+++ asgn5/.WRITEUP/plot.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 cd ../ && make clean all
 typeset -i n
-echo "" > /tmp/${2}_entropy.dat
+echo "" > /tmp/entropy.dat
+echo "" > /tmp/entropy_encoded.dat
 for fp in $(seq 0 0.001 1); do
-	ENTROPY=$(./encode < $1 | ./error -e $fp | ./entropy)
-	echo "$fp $ENTROPY" >> /tmp/${2}_entropy.dat
+	ENTROPY=$(cat $1 | ./error -e $fp | ./entropy)
+	ENTROPY_ENCODED=$(./encode < $1 | ./error -e $fp | ./entropy)
+	echo "$fp $ENTROPY" >> /tmp/entropy.dat
+	echo "$fp $ENTROPY_ENCODED" >> /tmp/entropy_encoded.dat
 done
 make clean & cd .WRITEUP/graphs
 gnuplot <<XX
 set terminal jpeg size 900, 600
 set pointsize 0.2
-set title "Entropy vs Noise"
+set title "Entropy vs Noise in ${2}"
 set xlabel "Percent Noise"
 set ylabel "Entropy"
 show title
 show xlabel
 show ylabel
-# set xrange [0:1000]
+set xtics 0,0.1,1
 
 set key top right
 
-set output "${2}_entropy.jpg"
-plot "/tmp/${2}_entropy.dat" with points lc rgbcolor "#46237A" title "${2} file entropy"
+set output "${2}.entropy.jpg"
+plot "/tmp/entropy.dat" with points lc rgbcolor "#46237A" title "original entropy", \
+	 "/tmp/entropy_encoded.dat" with points lc rgbcolor "#1ed448" title "encoded entropy"
+	
